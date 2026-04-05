d038a7adf1a79c05bbc4c51c865d7746fb8c1db7
Sakthi Shri T <sakthishri004@gmail.com>
2026-04-05 10:31:09 +0530

    Configure demo launch with server settings

--- app.py
+++ app.py
@@ -100,4 +100,7 @@ with gr.Blocks(title="Guava Quality Predictor", css=css,
     predict_btn.click(fn=predict, inputs=image_input, outputs=outputs)
 
 if __name__ == "__main__":
-    demo.launch()
+    demo.launch(
+        server_name="0.0.0.0",
+        server_port=int(os.environ.get("PORT", 10000))
+    )
